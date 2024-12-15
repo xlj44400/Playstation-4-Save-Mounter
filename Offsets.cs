41562c5a492d9d258bc052240f3cbb88bc055f47
idlesauce <148508202+idlesauce@users.noreply.github.com>
2024-12-15 21:22:10 +0100

    Add 2.50 offsets

--- Offsets.cs
+++ Offsets.cs
@@ -4,11 +4,12 @@ namespace PS4Saves;
 
 public static class Offsets
 {
-    public static readonly string[] Firmwares = ["4.03", "4.50","5.xx"];
+    public static readonly string[] Firmwares = ["2.50", "4.03", "4.50","5.xx"];
     public static string SelectedFirmware { get; set; } = string.Empty; // updated by fwVersionComboBox
   
     public static ulong sceUserServiceGetInitialUser => SelectedFirmware switch
     {
+        "2.50" => 0x32E0,
         "4.03" => 0x3290,
         "4.50" => 0x3290,    // offsets for both 4.03 and 4.50 are same
         "5.xx" => 0x33B0,
@@ -17,6 +18,7 @@ public static class Offsets
 
     public static ulong sceUserServiceGetLoginUserIdList => SelectedFirmware switch
     {
+        "2.50" => 0x2A50,
         "4.03" => 0x2A50,
         "4.50" => 0x2A50,    
         "5.xx" => 0x2B00,
@@ -25,6 +27,7 @@ public static class Offsets
 
     public static ulong sceUserServiceGetUserName => SelectedFirmware switch
     {
+        "2.50" => 0x46A0,
         "4.03" => 0x46E0,
         "4.50" => 0x46E0,    
         "5.xx" => 0x4830,
@@ -33,6 +36,7 @@ public static class Offsets
 
     public static ulong sceSaveDataMount2 => SelectedFirmware switch
     {
+        "2.50" => 0x2FE00,
         "4.03" => 0x31470,
         "4.50" => 0x31470,    
         "5.xx" => 0x321B0,
@@ -41,6 +45,7 @@ public static class Offsets
 
     public static ulong sceSaveDataUmount => SelectedFirmware switch
     {
+        "2.50" => 0x302D0,
         "4.03" => 0x31940,
         "4.50" => 0x31940,    
         "5.xx" => 0x32680,
@@ -49,6 +54,7 @@ public static class Offsets
 
     public static ulong sceSaveDataDirNameSearch => SelectedFirmware switch
     {
+        "2.50" => 0x310B0,
         "4.03" => 0x32720,
         "4.50" => 0x32720,    
         "5.xx" => 0x33460,
@@ -57,6 +63,7 @@ public static class Offsets
 
     public static ulong sceSaveDataTransferringMount => SelectedFirmware switch
     {
+        "2.50" => 0x30180,
         "4.03" => 0x317F0,
         "4.50" => 0x317F0,    
         "5.xx" => 0x32530,
@@ -65,6 +72,7 @@ public static class Offsets
 
     public static ulong sceSaveDataInitialize3 => SelectedFirmware switch
     {
+        "2.50" => 0x2F970,
         "4.03" => 0x30FE0,
         "4.50" => 0x30FE0,    
         "5.xx" => 0x31D20,
